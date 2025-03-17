393bbcedec96b1a998e6e24677d3e5e5b457dc44
Cristhian1811 <mv.cristhian18@hotmail.com>
2025-03-16 21:28:30 -0500

    inicial

---
+++ src/main/java/com/example/ec3mejiachero/Ec3MejiaCheroApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec3mejiachero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3MejiaCheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3MejiaCheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ec3mejiachero/Ec3MejiaCheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec3mejiachero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3MejiaCheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
